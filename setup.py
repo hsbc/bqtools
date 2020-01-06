166ab757d26ee24740c173d0ffc08af59f6ff2cb
Mike Moore <z_z_zebra@yahoo.com>
2020-01-06 23:27:28 +0000

    Added access copying or not

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setuptools.setup(
     name='bqtools-json',
-    version='0.4.40',
+    version='0.4.41',
     author="Mike Moore",
     author_email="z_z_zebra@yahoo.com",
     description="A Big Query json utility package",
