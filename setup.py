387d5a9b72c98e37f67b63a23d35821a8dd44706
Mike Moore <z_z_zebra@yahoo.com>
2020-02-02 15:57:01 +0000

    #9 Add referenced projects and datsets support

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setuptools.setup(
     name='bqtools-json',
-    version='0.4.67',
+    version='0.4.68',
     author="Mike Moore",
     author_email="z_z_zebra@yahoo.com",
     description="A Big Query json utility package",
